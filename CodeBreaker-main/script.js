6723a570b3410f41a72b482f9d431d1904281753
alany12 <114016247+alany12@users.noreply.github.com>
2022-12-01 13:14:42 -0500

    Update script.js

--- CodeBreaker-main/script.js
+++ CodeBreaker-main/script.js
@@ -1,117 +1,82 @@
 //Alan Yuan Period 7-8 even 11/22/22 CodeBreaker
-function initialize(){
-    clock = document.getElementById("clock");
-    display = document.getElementById("display");
-    logList = document.getElementById("log");
-    button1 = document.getElementById("button1");
-    button2 = document.getElementById("button2");
-    button3 = document.getElementById("button3");
-    clock.innerHTML = 7;
-    //I can probably delete b1 and just use button1 directly instead etc
-    b1 = 0;
-    b2 = 0;
-    b3 = 0;
-    logList.innerHTML = "Previous Guesses:";
-    code = '';
-    userCode = '' + b1 + b2 + b3;
-    display.innerHTML = userCode;
-    createCode();
-    
-    //console.log(userCode);
-    //console.log(code);
-}
-//Creates the hidden Code
-function createCode(){
-    for(let i = 0 ; i < 3 ; i++){
-        code += Math.floor((Math.random() * 10));
-    }
-}
-//Updates the userCode
-function changeUserCode(){
-    userCode = '' + b1 + b2 + b3
-    display.innerHTML = userCode;
+var playerGuessNum = "";
+var correctGuess = "";
+var guessesLeft = 7;
+const higher = " -  code is higher";
+const lower = " - code is lower";
+const correct = " - Correct!"
+const gameOver = " - Game Over! The correct code was "
+function initialize() {
+    playerGuessDisplay = document.getElementById("playerGuess");
+    playerLog = document.getElementById("log");
+    playerGuessesLeftDisplay = document.getElementById("clock");
+
+    generateNumber();
 }
 
-//checks if userCode is the same as the hidden code
-function checkCode(){
-    console.log(userCode);
-    console.log(code);
-    if(userCode == code)
-    {
-        display.innerHTML = "You've cracked the Code!"
-        console.log("nice");
+function generateNumber() {
+    let digit1 = Math.floor((Math.random() * 3) + 1);
+    digit1 = digit1.toString();
+    let digit2 = Math.floor((Math.random() * 3) + 1);
+    digit2 = digit2.toString();
+    let digit3 = Math.floor((Math.random() * 3) + 1);
+    digit3 = digit3.toString();
 
-    }
-    else{
-        clock.innerHTML -= 1;
-        if(clock.innerHTML == 0)
-        {
-            clock.innerHTML = "You've Ran out Of Guesses (Press Reset)"
-            display.innerHTML = "The code was " + code; 
-            console.log(clock);
+    correctGuess = digit1 + digit2 + digit3;
+}
+
+function guessOne() {
+    playerGuessNum += "1";
+    display();
+}
+function guessTwo() {
+    playerGuessNum += "2";
+    display();
+}
+function guessThree() {
+    playerGuessNum += "3";
+    display();
+}
+function testGuess() {
+    guessesLeft--;
+    if (playerGuessNum === correctGuess) {
+        playerLog.innerHTML += playerGuessNum + correct;
+        guessesLeft = 7;
+        generateNumber();
+    } else if (parseInt(playerGuessNum) > parseInt(correctGuess)) {
+        if (guessesLeft < 1) {
+            playerLog.innerHTML += playerGuessNum + gameOver + correctGuess;
+            guessesLeft = 7;
+            generateNumber();
+        } else {
+            playerLog.innerHTML += playerGuessNum + lower;
         }
-        else{
-            log();
+    } else {
+        if (guessesLeft < 1) {
+            playerLog.innerHTML += playerGuessNum + gameOver + correctGuess;
+            guessesLeft = 7;
+            generateNumber();
+        } else {
+            playerLog.innerHTML += playerGuessNum + higher;
         }
     }
+    playerGuessesLeftDisplay.innerHTML = "Guesses Left: " + guessesLeft;
+    playerLog.innerHTML += "<br>";
+}
+function display() {
+    playerGuessDisplay.innerHTML = playerGuessNum;
+    if (playerGuessNum.length < 3) {
+        for (let i = playerGuessNum.length; i < 3; i++) {
+            playerGuessDisplay.innerHTML += "-";
+        }
+    } else {
+        testGuess();
+        playerGuessNum = "";
+        display();
+    }
 }
 
-//Logs the user guesses
-function log(){
-    logList.innerHTML += "<br>" + userCode;
-}
-
-//Clears the user Choices
-function clearUserCode(){
-    b1 = 0;
-    b2 = 0;
-    b3 = 0;
-    button1.innerHTML = 0;
-    button2.innerHTML = 0;
-    button3.innerHTML = 0;
-    changeUserCode();
-}
-
-//CHANGES VALUE OF BUTTONS
-function changeValue1(){
-    if(b1 != 9)
-    {
-        b1 += 1;
-    }
-    else{
-        b1 = 0;
-    }
-    button1.innerHTML = b1;
-    changeUserCode();
-}
-
-function changeValue2(){
-    if(b2 != 9)
-    {
-        b2 += 1;
-    }
-    else{
-        b2 = 0;
-    }
-    button2.innerHTML = b2;
-    changeUserCode();
-}
-
-function changeValue3(){
-    if(b3 != 9)
-    {
-        b3 += 1;
-    }
-    else{
-        b3 = 0;
-    }
-    button3.innerHTML = b3;
-    changeUserCode();
-}
-
-function reset(){
-    initialize();
-    button1.innerHTML = "0";
-    button2.innerHTML = "0";
-    button3.innerHTML = "0";
+function clearAll() {
+    playerGuessNum = "";
+    display();
 }
