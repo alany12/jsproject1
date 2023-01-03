c0a65f285579c72d1c6f792c185eba30628b6e19
alany12 <114016247+alany12@users.noreply.github.com>
2023-01-03 13:48:54 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,82 @@
+//Alan Yuan Period 7-8 even 11/22/22 CodeBreaker
+var playerGuessNum = "";
+var correctGuess = "";
+var guessesLeft = 7;
+const higher = " -  code is higher";
+const lower = " - code is lower";
+const correct = " - Correct!"
+const gameOver = " - Game Over! The correct code was "
+function initialize() {
+    playerGuessDisplay = document.getElementById("playerGuess");
+    playerLog = document.getElementById("log");
+    playerGuessesLeftDisplay = document.getElementById("clock");
+
+    generateNumber();
+}
+
+function generateNumber() {
+    let digit1 = Math.floor((Math.random() * 3) + 1);
+    digit1 = digit1.toString();
+    let digit2 = Math.floor((Math.random() * 3) + 1);
+    digit2 = digit2.toString();
+    let digit3 = Math.floor((Math.random() * 3) + 1);
+    digit3 = digit3.toString();
+
+    correctGuess = digit1 + digit2 + digit3;
+}
+
+function guessOne() {
+    playerGuessNum += "1";
+    display();
+}
+function guessTwo() {
+    playerGuessNum += "2";
+    display();
+}
+function guessThree() {
+    playerGuessNum += "3";
+    display();
+}
+function testGuess() {
+    guessesLeft--;
+    if (playerGuessNum === correctGuess) {
+        playerLog.innerHTML += playerGuessNum + correct;
+        guessesLeft = 7;
+        generateNumber();
+    } else if (parseInt(playerGuessNum) > parseInt(correctGuess)) {
+        if (guessesLeft < 1) {
+            playerLog.innerHTML += playerGuessNum + gameOver + correctGuess;
+            guessesLeft = 7;
+            generateNumber();
+        } else {
+            playerLog.innerHTML += playerGuessNum + lower;
+        }
+    } else {
+        if (guessesLeft < 1) {
+            playerLog.innerHTML += playerGuessNum + gameOver + correctGuess;
+            guessesLeft = 7;
+            generateNumber();
+        } else {
+            playerLog.innerHTML += playerGuessNum + higher;
+        }
+    }
+    playerGuessesLeftDisplay.innerHTML = "Guesses Left: " + guessesLeft;
+    playerLog.innerHTML += "<br>";
+}
+function display() {
+    playerGuessDisplay.innerHTML = playerGuessNum;
+    if (playerGuessNum.length < 3) {
+        for (let i = playerGuessNum.length; i < 3; i++) {
+            playerGuessDisplay.innerHTML += "-";
+        }
+    } else {
+        testGuess();
+        playerGuessNum = "";
+        display();
+    }
+}
+
+function resetAll() {
+    playerGuessNum = "";
+    display();
+}
